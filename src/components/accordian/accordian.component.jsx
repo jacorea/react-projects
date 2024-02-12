6818062d7a048ae034b4411d86365c33f7e3a03f
Jonathan Corea <jonathan@3plwinner.local>
2024-02-12 10:16:34 -0800

    Add Accordion component. Need to move MultiSelection button to the buttom. It would be better to toggle on/off

---
+++ src/components/accordian/accordian.component.jsx
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import data from "./data";
+import "./accordian.styles.css"
+
+const Accordian = () => {
+    const [selected, setSelected] = useState(null);
+    const [enableMultiSelection, setEnableMultiSelection] = useState(false);
+    const [multiple, setMultiple] = useState([]);
+
+    const handleSingleSelection = (getCurrentId) => {
+        setSelected(getCurrentId === selected ? null: getCurrentId)
+    }
+
+    const handleMultiSelection = (getCurrentId) => {
+        let cpyMultiple = [...multiple];
+        const findIndexOfCurrentId = cpyMultiple.indexOf(getCurrentId);
+        console.log(findIndexOfCurrentId)
+
+        if (findIndexOfCurrentId === -1) cpyMultiple.push(getCurrentId);
+        else cpyMultiple.splice(findIndexOfCurrentId, 1);
+
+        setMultiple(cpyMultiple)
+    }
+
+    console.log('selected', selected, multiple)
+
+    return (
+        <div className="acc-wrapper">
+            <button onClick={()=>setEnableMultiSelection(!enableMultiSelection)}>
+                Enable Multi Select
+            </button>
+            <div className="accordian">
+                {data && data.length > 0 ? (
+                    data.map((dataItem)=>(
+                        <div className="item">
+                            <div onClick={enableMultiSelection? ()=>handleMultiSelection(dataItem.id) :()=>handleSingleSelection(dataItem.id)} className="title">
+                                <h3>{dataItem.question}</h3>
+                                <span>+</span>
+                            </div>
+                            {   enableMultiSelection ? 
+                                    multiple.indexOf(dataItem.id) !== -1 && (
+                                        <div className="acc-content ">
+                                            {dataItem.answer}
+                                        </div>
+                                    )
+
+                                : selected === dataItem.id ? 
+                                <div className="acc-content ">
+                                    {dataItem.answer}
+                                </div>
+                                : null
+                            }
+                        </div>
+                    ))
+                ): (
+                    <div>
+                        no data found
+                    </div>
+                )}
+            </div>
+        </div>
+    )
+}
+
+export default Accordian
